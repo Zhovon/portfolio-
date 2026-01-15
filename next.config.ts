2238e9d0c85bad0472ee70f20e826f508d377cab
Zhovon <shahadathoss01@gmail.com>
2026-01-16 00:12:22 +0600

    Add graphql dependency and webpack config

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,12 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.resolve.extensionAlias = {
+      '.js': ['.js', '.ts', '.tsx'],
+    }
+    return config
+  },
 };
 
 export default withPayload(nextConfig);
