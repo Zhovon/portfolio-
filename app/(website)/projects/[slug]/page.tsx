54ac3d9c970ef76804ccccb0369d0937175eacc4
Zhovon <shahadathoss01@gmail.com>
2026-02-01 20:02:16 +0600

    fix: add generateStaticParams for project detail pages

--- app/(website)/projects/[slug]/page.tsx
+++ app/(website)/projects/[slug]/page.tsx
@@ -1,6 +1,13 @@
 import { CaseStudyClient } from './CaseStudyClient'
 import { notFound } from 'next/navigation'
-import { getProjectBySlug } from '@/data/projects'
+import { getProjectBySlug, projects } from '@/data/projects'
+
+// Generate static params for all projects
+export function generateStaticParams() {
+    return projects.map((project) => ({
+        slug: project.slug,
+    }))
+}
 
 export default function CaseStudyPage({ params }: { params: { slug: string } }) {
     const { slug } = params
