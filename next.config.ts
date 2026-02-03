023ed150dd8d31f325a13e6f006ec906e38896d1
Zhovon <shahadathoss01@gmail.com>
2026-02-03 22:13:44 +0600

    fix: next config image domains

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   // Force webpack instead of Turbopack
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
   experimental: {
     webpackBuildWorker: true,
   },
